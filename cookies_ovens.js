a36f50e7de90e81c1f1941ff72f90170fc8b2b33
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-03 10:49:26 -0500

    added a cookie to the prep table

--- cookies_ovens.js
+++ cookies_ovens.js
@@ -1,29 +1,42 @@
 function Batch(type, requiredBakeTime){
   this.type = type
   this.requiredBakeTime = requiredBakeTime
-
+  Table.addBatches(this)
 }
 
+var Table = {
 
+  init: function() {
+    this.pendingBatches = new Array()
+  },
 
+  addBatches: function(batch){
+    this.pendingBatches.push(batch)
+    //append a batch
+  }
+}
+
+//when we create a batch we append a li of the batch type to table
+//when appended to table, table needs an add to oven button
+//when add to oven clicked, which adds the cookie to the oven
 
 
 
 
 $(document).ready(function() {
 
+  Table.init()
+
   $('#new_batch').submit(function(event) {
     event.preventDefault()
     type = $('#type').val()
-    time = $('#time').val()
+    time = parseInt($('#time').val())
 
     var batch = new Batch(type, time)
 
+    console.log(Table)
 
     //we want to get the params of the form
     // and create a cookie object using those params
-
-
   })
-
 })
