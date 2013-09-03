7a07ec90e25b65d6ec8573bdb204003f6f9b4c3a
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-03 10:06:06 -0500

    created a cookie

--- cookies_ovens.js
+++ cookies_ovens.js
@@ -1,4 +1,4 @@
-function Cookie(type, requiredBakeTime){
+function Batch(type, requiredBakeTime){
   this.type = type
   this.requiredBakeTime = requiredBakeTime
 
@@ -14,6 +14,12 @@ $(document).ready(function() {
 
   $('#new_batch').submit(function(event) {
     event.preventDefault()
+    type = $('#type').val()
+    time = $('#time').val()
+
+    var batch = new Batch(type, time)
+
+
     //we want to get the params of the form
     // and create a cookie object using those params
 
