7f11a427cfc0dc3685cc34681ef507d3223ea39d
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-03 09:45:57 -0500

    initial commit

---
+++ cookies_ovens.js
@@ -0,0 +1,23 @@
+function Cookie(type, requiredBakeTime){
+  this.type = type
+  this.requiredBakeTime = requiredBakeTime
+
+}
+
+
+
+
+
+
+
+$(document).ready(function() {
+
+  $('#new_batch').submit(function(event) {
+    event.preventDefault()
+    //we want to get the params of the form
+    // and create a cookie object using those params
+
+
+  })
+
+})
