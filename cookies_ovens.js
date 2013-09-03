850d0e11f8f21a03d438b1f204041fe88e32f6f3
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-03 15:30:27 -0500

    added id tags to html elements

--- cookies_ovens.js
+++ cookies_ovens.js
@@ -1,11 +1,15 @@
+
 var Batch = {
 
   init: function(){
     this.listenForCreateEvent()
+    this.counter = 1
   },
 
   createBatch: function(type, time){
     var batch = {type: type, requiredBakeTime: time, timeBaked: 0}
+    batch.id = this.counter
+    this.counter += 1
     Table.addBatches(batch)
   },
 
@@ -25,19 +29,31 @@ var Table = {
 
   init: function() {
     this.pendingBatches = new Array()
+
+    $('body').on('submit', '.batch', function(event) {
+      event.preventDefault()
+
+      // console.log(Oven.racks)
+      // console.log(Table.pendingBatches)
+      batch = Table.pendingBatches.shift()
+      Oven.racks.push(batch)
+      console.log(Oven.racks)
+    });
+
   },
 
   addBatches: function(batch){
     this.pendingBatches.push(batch)
-    $('#prep_batches').append('<li>' + batch.type + '<input class="batch" type="submit" value="Add to oven"> </li>')
+    $('#prep_batches').append('<li>' + batch.type + '<form data-id="' + batch.id + '" class="batch"> <input type="submit" value="Add to oven"> </form></li>')
   }
 }
 
 var Oven = {
 
   init: function() {
-    this.trays
-  }
+    this.racks = new Array()
+  },
+
 
 }
 //when we create a batch we append a li of the batch type to table
